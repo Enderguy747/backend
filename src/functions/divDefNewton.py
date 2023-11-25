e90d3a69641161788a73cc7f4d6ff451592b0372
Jose Daniel Rodriguez Sotela <jo.sed98@hotmail.com>
2023-11-24 22:55:14 -0600

    grafica lista

--- src/functions/divDefNewton.py
+++ src/functions/divDefNewton.py
@@ -1,7 +1,9 @@
 
-from sympy import symbols, simplify
-
+from shutil import which
+from sympy import simplify, symbols , lambdify
+import matplotlib.pyplot as plt
 import numpy as np
+
 
 x = symbols('x')
 
@@ -35,10 +37,12 @@ def divDefNewton(xAxis, yAxis):
     
     
     print('\n valores de los terminos: ')
+    termValues = []
     for l in range(len(ddTable[0])):
         print("a [",l,"] =",ddTable[l+1][0])
-
-
+        termValues.append(ddTable[l+1][0])
+    print('\nLista de valores')
+    print(termValues)
 
     print('\n')
     matrix=np.array(ddTable)
@@ -59,12 +63,39 @@ def divDefNewton(xAxis, yAxis):
     print(p)    
     print('\n polinomio simplificado')
     print(simplify(p))
-        
     
+    ##grafic settings
+
+    px = lambdify(x,simplify(p))
+    graficScale = len(xAxis)*10
+    pointA = np.min(xAxis)
+    pointB = np.max(xAxis)
+    pXAxis = np.linspace(pointA,pointB,graficScale)
+    pYAxis = px(pXAxis)
+    
+    ##grafic draw
+    
+    plt.plot(pXAxis,pYAxis,'o', label = 'Puntos')
+
+    plt.plot(pXAxis,pYAxis, label = 'Polinomio')
+    plt.legend()
+    plt.grid(True)
+    plt.xlabel('pXAxis')
+    plt.ylabel('pYAxis')
+    plt.title('Diferencias Divididas - Newton')
+    plt.show()
+    
+ 
+        
+    return p,simplify(p),termValues
 
 
 
 
-m=[18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
-n=[16,22,12,13,10,8,8,6,7,5,6,4,3,42,2,2]
+##m=[18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
+##n=[16,22,12,13,10,8,8,6,7,5,6,4,3,42,2,2]
+##m=[-4,-1,2,5,7]
+##n=[4,1,2,5,7]
+m=[3.2, 3.8, 4.2, 4.5]
+n=[5.12, 6.42, 7.25, 6.85]
 divDefNewton(m,n)
--- src/functions/hello.py
+++
@@ -1,7 +0,0 @@
-##this is an example
-
-## def sayYourName(name):
-##    return("your name is "+name)
-
-## def tryThis():
-##    print("works")
