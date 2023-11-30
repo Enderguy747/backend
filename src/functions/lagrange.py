61f57e12a9de66d612b77cd7a377f7d3891f3f16
Jose Daniel Rodriguez Sotela <jo.sed98@hotmail.com>
2023-11-29 23:43:41 -0600

    lagrange listo y api funcionando

---
+++ src/functions/lagrange.py
@@ -0,0 +1,48 @@
+
+import numpy as np
+from sympy import lambdify, symbols, simplify
+import matplotlib.pyplot as plt
+
+
+x = symbols("x")
+
+def polinomioFactory (xi,yi):
+    polinomio = 0 
+    for i in range(len(xi)):
+        numerador = 1
+        denominador =1
+        for j in range(len(xi)):
+            if j!=i:
+                numerador*=(x-xi[j])
+                denominador*=(xi[i]-xi[j])
+        terminoL = numerador/denominador
+        polinomio+=terminoL*yi[i]
+    return polinomio
+    
+def valoresDelPolinomio(xi,yi):
+    polinomio=polinomioFactory(xi,yi)
+    polinomioSimplificado=simplify(polinomio)
+    px=lambdify(x,polinomioSimplificado)
+    muestras=100## tamaño de muestras
+    a,b=min(xi),max(xi)
+    pxi=np.linspace(a,b,muestras)
+    pyi=px(pxi)
+    return pxi.tolist(),pyi.tolist(),str(polinomio),str(polinomioSimplificado)
+
+def grafico(xi,yi,pxi,pyi):
+    plt.plot(xi,yi,"o",label="puntos")
+    plt.plot(pxi,pyi,label="polinomio")
+    plt.legend()
+    ##plt.grid(1)
+    plt.xlabel('xi')
+    plt.ylabel('yi')
+    plt.title("interpolacion de lagrange")
+    plt.show()
+xi=np.array([0,6,10,13,17,20,28])
+yi=np.array([6.67,17.33,42.97,37.33,30.10,29.31,28.74])
+    
+##xi=[1,3,5,7]
+##yi=[-2,1,2,-3]
+##pxi,pyi,polinomio,polinomioSimplificado=valoresDelPolinomio(xi,yi)
+    
+##print(polinomioSimplificado)
--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 #import routes here
-from src.routes import routesDivDefNewton
+from src.routes import routesDivDefNewton,routesLaGrange
 
 #use routes here
 #app.include_router(index.router)
 app.include_router(routesDivDefNewton.router)
+app.include_router(routesLaGrange.router)
 
 
 #default home api
