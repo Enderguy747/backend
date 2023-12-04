22d3ce6c048376188c6cae78fba717593660cdb0
Jose Daniel Rodriguez Sotela <jo.sed98@hotmail.com>
2023-12-03 21:40:42 -0600

    rutas funcionando

---
+++ src/models/LaGrange.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+class LaGrange(BaseModel):
+    xi:list
+    yi:list
+    
--- src/routes/routesLaGrange.py
+++ src/routes/routesLaGrange.py
@@ -1,25 +1,20 @@
 from fastapi import APIRouter
+from ..models.LaGrange import LaGrange
 from ..functions.lagrange import valoresDelPolinomio as vdp
 
 router = APIRouter()
-xi=[0,6,10,13,17,20,28]
-yi=[6.67,17.33,42.97,37.33,30.10,29.31,28.74]
+##ejemplo de query
+##xi=[0,6,10,13,17,20,28]
+##yi=[6.67,17.33,42.97,37.33,30.10,29.31,28.74]
 
-@router.post("/lagrange/{coordenadas}")
-
-async def diferenciasDivididas(coordenadas):
-    ##valuesArray=(coordenadas)
-    
-    ##print(valuesArray)
-    ##pxi,pyi,polinomio,polinomioSimplificado=vdp(xi,yi)
+@router.post("/lagrange")
+def diferenciasDivididas(coordenadas:LaGrange):
+  
+    pxi,pyi,polinomio,polinomioSimplificado=vdp(coordenadas.xi,coordenadas.yi)
    
-    print(coordenadas)
-    return coordenadas
-    ''''
     return {
         'polinomio':polinomio,
         'polinomioSimplificado':polinomioSimplificado,
         'valoresDeX':pxi,
         'valoresDey':pyi
         }
-'''
