fea381f640396c4de388aae098079020282ec02c
Jose Daniel Rodriguez Sotela <61768197+Enderguy747@users.noreply.github.com>
2023-12-05 10:38:48 -0600

    Actualizar main.py

--- src/main.py
+++ src/main.py
@@ -11,9 +11,9 @@ origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
-    "http://localhost:8080",
+    "http://:8080",
 '''
-"*"
+"http://127.0.0.1:8000"
 ]
 
 app.add_middleware(
